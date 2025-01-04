7fddc84aab7afe66e01ad8be186cf32654cee324
Mark Devenyi <markdevenyidev@gmail.com>
2025-01-04 00:08:26 -0500

    feat(core): add terminal (`:term`) keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,3 +3,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Terminal keybinds
+vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
+vim.api.nvim_set_keymap('t', '<C-w><C-w>', [[<C-\><C-n><C-w>w]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>t', ':vsp | term<CR>', { noremap = true, silent = true })
