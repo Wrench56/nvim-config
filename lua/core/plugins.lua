781655f03495259248b17ccef94130f553316381
Mark Devenyi <markdevenyidev@gmail.com>
2025-01-03 23:58:14 -0500

    feat(plugins): add `nvim-web-devicons` as `nvim-tree` dependency

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -3,5 +3,8 @@
 return {
     "navarasu/onedark.nvim",
     "nvim-telescope/telescope.nvim",
-    "nvim-tree/nvim-tree.lua"
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {"nvim-tree/nvim-web-devicons"}
+    }
 }
