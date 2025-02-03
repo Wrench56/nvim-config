ff99aabb2d670ebc6734a1bd44082d0b23ffc42a
Mark Devenyi <markdevenyidev@gmail.com>
2025-02-02 23:18:23 -0500

    feat(core): configure `<Tab>` and indentation behaviour

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -6,3 +6,10 @@ vim.g.loaded_netrwPlugin = 1
 
 -- Enable line number
 vim.wo.number = true
+
+-- Configure <Tab> behaviour
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
