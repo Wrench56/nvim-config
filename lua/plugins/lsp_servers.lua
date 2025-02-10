27d6d65c2bdcea0cef238480b64d378e7e915698
Mark Devenyi <markdevenyidev@gmail.com>
2025-02-10 00:31:24 -0500

    feat(plugins): add `rust-analyzer` initialization

--- lua/plugins/lsp_servers.lua
+++ lua/plugins/lsp_servers.lua
@@ -1,3 +1,15 @@
 -- [[ LSP servers ]]
 
 require('lspconfig').asm_lsp.setup({})
+require('lspconfig').rust_analyzer.setup({
+    settings = {
+        ['rust-analyzer'] = {
+            check = {
+                command = "clippy";
+            },
+            diagnostics = {
+                enable = true;
+            }
+        }
+    }
+})
