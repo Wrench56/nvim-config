4d44177c69563851e94dec560711ff052e2a04fa
Mark Devenyi <markdevenyidev@gmail.com>
2025-08-09 18:23:16 -0400

    feat(core): add diagnostic keymaps for buffer and workspace

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,5 +8,16 @@ vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
 vim.api.nvim_set_keymap("t", "<C-w><C-w>", [[<C-\><C-n><C-w>w]], { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>t", ":vsp | term<CR>a", { noremap = true, silent = true })
 
+-- Diagnostics
+-- Local buffer
+vim.keymap.set("n", "<leader>p", function()
+  vim.diagnostic.setloclist()
+end, { noremap = true, silent = true, desc = "Show buffer diagnostics" })
+
+-- Workspace
+vim.keymap.set("n", "<leader>P", function()
+  require("telescope.builtin").diagnostics()
+end, { noremap = true, silent = true, desc = "Search workspace diagnostics" })
+
 -- Lazygit
 vim.api.nvim_set_keymap("n", "<leader>g", ":LazyGit<CR>", { noremap = true, silent = true })
